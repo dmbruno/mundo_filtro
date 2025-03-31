2c638239246e91d5e8a3d5b7014813b2c485d793
Diego Bruno <dmbruno61@gmail.com>
2025-03-31 16:36:26 -0300

    fix url

--- backend/models/usuarios.py
+++ backend/models/usuarios.py
@@ -6,7 +6,7 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     is_admin = db.Column(db.Boolean, default=False) 
 
     def set_password(self, password):
